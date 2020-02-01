b82e9126216bc7442bb88fd0294fa5472820d00a
Santosh Mohanty <Santosh.Mohanty@cerner.com>
2020-02-01 10:46:57 +0530

    increment counter

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
-export default () => "Let's go shopping";
+import React, {Component} from 'react';
+
+
+class App extends Component {
+  state = {
+    counter: 0
+  }
+
+incrementCounter = () => {
+  const oldCounter = this.state.counter;
+  this.setState({
+    counter: oldCounter + 1
+  });
+}
+  render() {
+    return <div className="counter">
+      <button className="counter-btn">-</button>
+      <div className="counter-text">{this.state.counter}</div>
+      <button className="counter-btn" onClick={this.incrementCounter}>+</button>
+    </div>
+  }
+}
+export default App;
